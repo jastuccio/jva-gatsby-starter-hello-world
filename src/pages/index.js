885f2e22d12ff927a6adb9116f8a4226cddc8a2c
Joe <joe@astucc.io>
2019-04-13 12:03:33 -0400

    Add Styled Components, ESLint, and Prettier

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,9 @@
 import React from "react"
+import styled from 'styled-components';
 
-export default () => <div>Hello world!</div>
+export default () => <Test>Hello world!</Test>
+
+
+const Test = styled.div`
+color: red;
+`
