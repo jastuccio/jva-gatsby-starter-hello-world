e3972f26f0e865bddf8850edaf20041402d2de13
Joe <joe@astucc.io>
2019-04-21 18:06:38 -0400

    feat: Add siteMetadata to gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,27 @@
 module.exports = {
+  siteMetaData: {
+    title: 'site title',
+    description: 'site description',
+    author: 'Joe Astuccio',
+  },
   plugins: [
     {
       resolve: `gatsby-plugin-styled-components`,
       options: {
         // Add any options here
+      },
+    },
+      {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: '',
+        short_name: '',
+        start_url: '/',
+        background_color: '',
+        theme_color: '',
+        display: 'minimal-ui',
+        // icon: 'src/images/gatsby-icon.png', // This path is relative to the root of the site.
+        theme_color_in_head: false, // This will avoid adding theme-color meta tag.
       },
     },
   ],
