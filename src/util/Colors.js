e27baa9d1b8df82dd1685513efbd41831d48919a
Joe <joe@astucc.io>
2019-04-21 17:49:58 -0400

    feat: Add gray shades to color variables

--- src/util/Colors.js
+++ src/util/Colors.js
@@ -1,7 +1,28 @@
-export const purple = "#524763";
-export const teal = "#82D8D8";
+import { hsl } from "graphql-tools/dist/schemaVisitor"
 
-export const social = {
-  twitter: "#00aced",
-  facebook: "#3b5998"
-};
+// Codes for color palette
+export const colors = {
+  black: "hsl(0, 0, 20)", //#2f2f2f
+  white: "hsl(0, 0%, 99%)", //#fcfcfc
+
+  // Gray shades
+  gray900: "hsl(215, 22, 17)", 
+  gray800: "hsl(215, 17, 30)",
+  gray700: "hsl(214, 11, 49)",
+  gray600: "hsl(215, 17, 63)",
+  gray500: "hsl(210, 23, 75)",
+  gray400: "hsl(208, 17, 83)",
+  gray300: "hsl(210, 24, 87)",
+  gray200: "hsl(207, 22, 90)",
+  gray100: "hsl(210, 17, 98)",
+
+  // Primary shades
+  // primary900: "hsl()",
+  // primaryBase: "hsl()",
+  // primary100: "hsl()",
+
+  //Secondary shades
+  // secondary900: "hsl()",
+  // secondaryBase: "hsl()",
+  // secondary100: "hsl()",
+}
