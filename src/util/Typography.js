e4933814bd28f1732fbe2b2cf45b17d5c6771b0a
Joe <joe@astucc.io>
2019-05-05 14:27:17 -0400

    refactor: Rename font size variables for clarity

--- src/util/Typography.js
+++ src/util/Typography.js
@@ -4,20 +4,19 @@
 // font-family: 'Germania One', cursive;
 // `;
 
-
 /**
  * Font Sizes
  */
 export const font = {
-  smallest: "font-size: '12px'",
-  small: "font-size: '14px'",
-  base: "font-size: '16px'",
-  s2: "font-size: '18px'",
-  s3: "font-size: '20px'",
-  s4: "font-size: '24px'",
-  s5: "font-size: '30px'",
-  s6: "font-size: '36px'",
-  s7: "font-size: '48px'",
-  s8: "font-size: '60px'",
-  s9: "font-size: '72px'"
+  fontSizeSmallest: "font-size: '12px'",
+  fontSizeSmall: "font-size: '14px'",
+  fontSizeBase: "font-size: '16px'",
+  fontSize2: "font-size: '18px'",
+  fontSize3: "font-size: '20px'",
+  fontSize4: "font-size: '24px'",
+  fontSize5: "font-size: '30px'",
+  fontSize6: "font-size: '36px'",
+  fontSize7: "font-size: '48px'",
+  fontSize8: "font-size: '60px'",
+  fontSize9: "font-size: '72px'",
 }
